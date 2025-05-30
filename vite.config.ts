6fee35683e3d8d301bf83fbb2eb2ca235a56dcdd
Hari Krishnan <rharikrishnan8726@gmail.com>
2025-05-31 00:25:56 +0530

    Push latest code

--- vite.config.ts
+++ vite.config.ts
@@ -1,15 +1,18 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react-swc";
 import path from "path";
+import { componentTagger } from "lovable-tagger";
 
-export default defineConfig(() => ({
-  base: "/Portfolio/", // 👈 Important for GitHub Pages
+// https://vitejs.dev/config/
+export default defineConfig(({ mode }) => ({
   server: {
     host: "::",
     port: 8080,
   },
   plugins: [
     react(),
+    mode === 'development' &&
+    componentTagger(),
   ].filter(Boolean),
   resolve: {
     alias: {
@@ -17,4 +20,3 @@ export default defineConfig(() => ({
     },
   },
 }));
-
